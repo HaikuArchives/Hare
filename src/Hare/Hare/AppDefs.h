bdcd2253b5e675dcc6daf35c6a53ac03a6e11857
scottmc <scottmc@81046345-d08b-466e-b3d4-a9165186f0a1>
2009-07-27 05:39:36 +0000

    Checkin renamed Hare folders
    git-svn-id: http://svn.osdrawer.net/hare@5 81046345-d08b-466e-b3d4-a9165186f0a1

---
+++ src/Hare/Hare/AppDefs.h
@@ -0,0 +1,20 @@
+#ifndef __APP_DEFS_H__
+#define __APP_DEFS_H__
+
+#define COMPANY "Originally created by FlipSide Software"
+#define COMPANY_WWW "http://dev.osdrawer.net/projects/show/hare"
+#define COMPANY_EMAIL "haiku@scottmc.com"
+#define APPLICATION "Hare"
+#define VERSION "1.0.8"
+
+#define WINDOW_FILE "Hare Window"
+#define SETTINGS "Hare Settings"
+#define ADD_ON_DIR "add-ons"
+
+#define SIGNATURE "application/x-vnd.osdrawer.hare"
+
+#define CDDA_MIME_TYPE "audio/x-cdda"
+
+#define SYSTEM_BEEP_ENCODING_DONE "Encoding Finished"
+
+#endif
